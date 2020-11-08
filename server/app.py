b8cbcf8dbc655a3ca63d01617f8835395ef6e2a7
FatimahAreola <fatimahyinka@gmail.com>
2020-11-08 12:17:09 -0500

    Multiprocessing
    Updated API request to use multiprocessing to improve speeds, unfortunately the fastest I could get it was about half the time it took previously. This still averages around 2 seconds.

--- server/app.py
+++ server/app.py
@@ -3,6 +3,10 @@ from flask_cors import CORS
 import requests
 import mysql.connector
 
+import multiprocessing
+from multiprocessing import Pool, current_process
+
+
 app = Flask(__name__)
 DEBUG = True
 app.config.from_object(__name__)
@@ -11,7 +15,7 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 @app.route('/api/hello')
 def hello():
     print('Hello Request Received')
-    return jsonify('Hello This is the Flask App')
+    return jsonify('Hello This is the Flask App3')
 
 
 
@@ -19,24 +23,23 @@ def hello():
 with the details of 8 pre-determined works of art so that they can be formatted for gameplay.
  """
 
-def fetchArtInformation(artObjectIDs):
+def fetchArtInformation(i):
  #Define index, and Cardset dictionary
-    idx=0
-    cardSet={}  
+    
+    idx=current_process()._identity[0]
 #This for loop goes through all IDs provided in the list of art pieces chosen and pulls the required information about them from the MET's API
-    for i in artObjectIDs:
-        apiResponse = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/objects/" + str(i))
-        artDetails = apiResponse.json()
+    apiResponse = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/objects/" + str(i))
+    artDetails = apiResponse.json()
 #We add another object to the Cardset dictionary for each art piece, containing info on Name, ObjectID, URL and status
-        cardSet[idx]={
-        'cardId': idx+1,
-        'ObjectID': str(i),
-        'artName': artDetails["title"],
-        'artURL': artDetails["primaryImage"],
-        'active':  False,
-        'status': False
-        }
-        idx+=1
+   # cardSet[idx]={
+    cardSet={
+    'cardId': idx,
+    'ObjectID': str(i),
+    'artName': artDetails["title"],
+    'artUrl': artDetails["primaryImage"],
+    'active':  False,
+    'status': False
+    }
 #Returns a nested Json object, with multiple artCards inside
     return (json.dumps(cardSet))
 
@@ -48,7 +51,12 @@ def pullMETAPI():
 #These are the ObjectIDs of 8 pieces we selected for this demo.
 #For future iterations of the game, these objectIDs will need to be selected by the system.
     artObjectIDs = [16577,436944,437879,436101,40081,437329,436840,435882]
-    return(fetchArtInformation(artObjectIDs))
+    p=Pool(len(artObjectIDs))
+    x=p.map(fetchArtInformation,artObjectIDs)
+    return(jsonify(x))
+    #return(fetchArtInformation(artObjectIDs))
+
+
 
 
 @app.route('/api/ping')
