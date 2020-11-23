56c2fb8f928fbb925a751a5384d689f4a738f511
FatimahAreola <fatimahyinka@gmail.com>
2020-11-22 20:51:39 -0500

    Update app.py
    temporary revert of changes until I can figure out a way to avoid using the loop

--- server/app.py
+++ server/app.py
@@ -122,10 +122,11 @@ def selectArt (selectedArtist):
     [752047,11865,11859,11862,11866,11860,11863,11872]]
     
     artists= ["Paul Gauguin", "Vincent van Gogh", "Rembrandt", "Asher Brown Durand", "Albert Bierstadt", "Paul Cézanne", "Auguste Edouart", "Frederic Remington"]
-    
-    if artistIdx == artists.index(selectedArtist)
-        artObjectIDs=artArray[artistIdx]
-        return (artObjectIDs)
+    for a in range(8):
+        if selectedArtist == artists[a]:
+            artistIdx=artists.index(artists[a]) 
+            artObjectIDs=artArray[artistIdx]
+            return (artObjectIDs)
     
     #If none of the listed artists have been selected, a random game is generated
     else:
